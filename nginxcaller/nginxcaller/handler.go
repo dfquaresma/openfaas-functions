9a289b2286f62ee3b42851e671252565451979fe
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-04-24 15:27:03 -0300

    Update nginxcaller

--- nginxcaller/nginxcaller/handler.go
+++ nginxcaller/nginxcaller/handler.go
@@ -7,6 +7,6 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	resp, _ := http.Get("http://127.0.0.1:80")
-	return fmt.Sprintf("Outcaller status: %s", resp.Status)
+	resp, _ := http.Get("http://127.0.0.1")
+	return fmt.Sprintf("Nginx request status: %s", resp.Status)
 }
